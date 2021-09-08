7266fae01aca2d0d84c3a46621324d504c87676b
kajikentaro <k.kentarou0412@gmail.com>
2021-09-08 13:18:09 -0700

    add messages and comments

--- mkshortcut.py
+++ mkshortcut.py
@@ -7,12 +7,15 @@ SAVE_DIR_PARENT = r"C:\Users\aaa\AppData\Roaming\Microsoft\Windows\Start Menu\Pr
 SAVE_DIR = os.path.join(SAVE_DIR_PARENT, "FreeSofts")
 if not os.path.exists(SAVE_DIR):
 	os.mkdir(SAVE_DIR)
-
+	print("Target folder was created")
+else :
+	print("Target folder already exists")
 #%%
+# Define making shortcut function
 def mkshortcut(target_file):
 	basename = os.path.basename(target_file)
 	filename = os.path.splitext(basename)[0]
-	save_path=os.path.join(SAVE_DIR, filename+ ".lnk")
+	save_path=os.path.join(SAVE_DIR_PARENT, filename+ ".lnk")
 	#WSHを生成
 	wsh=comtypes.client.CreateObject("wScript.Shell",dynamic=True)
 	#ショートカットの作成先を指定して、ショートカットファイルを開く。作成先のファイルが存在しない場合は、自動作成される。
@@ -33,12 +36,15 @@ def mkshortcut(target_file):
 	short.Save()
 
 #%%
-
+# executing
 f = open('lists.txt', 'r', encoding="utf-8")
 while 1:
 	line = f.readline()
-	line = line.replace('\n','')
 	if not line:
 		break
+	line = line.replace('\n','')
+	if not line:
+		continue
 	print(line)
 	mkshortcut(line)
+# %%
